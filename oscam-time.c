776d8a1bb037793cf151ad3df72a3092cd5bb36e
oscam-installer <oscam-installer@proton.me>
2025-06-01 23:50:59 +0200

    smod

--- oscam-time.c
+++ oscam-time.c
@@ -362,15 +362,4 @@ void cs_gettime(struct timespec *ts)
 	ts->tv_nsec = tv.tv_usec * 1000;
 	clock_type = CLOCK_TYPE_REALTIME;
 	return;
-#elif defined (__MACH__)
-// OS X does not have clock_gettime, use clock_get_time
-	clock_serv_t cclock;
-	mach_timespec_t mts;
-	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-	clock_get_time(cclock, &mts);
-	mach_port_deallocate(mach_task_self(), cclock);
-	ts->tv_sec = mts.tv_sec;
-	ts->tv_nsec = mts.tv_nsec;
-	clock_type = CLOCK_TYPE_REALTIME;
-	return;
 }
